58b543174374510a73f31cbce20befa1aedf76e0
Simon van Heeringen <simon.vanheeringen@gmail.com>
2013-07-15 11:40:52 +0200

    Updated tests

--- tests/test_merge_overlapping_exons.py
+++ tests/test_merge_overlapping_exons.py
@@ -11,6 +11,28 @@ def data():
         [0, 100, 200],
         [50, 50, 50],
     ])
+    
+    # Simple 2-exon overlap
+    ret.append([
+        [0, 100, 200],
+        [101, 50, 50],
+        [0, 200],
+        [150, 50],
+    ])
+    # All exons overlap
+    ret.append([
+        [0, 100, 200],
+        [101, 101, 50],
+        [0],
+        [250],
+    ])
+    # More than one overlap 
+    ret.append([
+        [0, 100, 200, 300, 400, 500, 600],
+        [101, 101, 50, 50, 101, 101, 50],
+        [0, 300, 400],
+        [250, 50, 250],
+    ])
     return ret
 
 def test_retrieve_transcripts(data):
