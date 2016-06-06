81a94f7109771eed1d7f78be6112d9f39fb902dd
Joris <jjm.vansteenbrugge@student.han.nl>
2016-06-06 16:33:25 +0200

    UTR extends both ways update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools.command.test import test as TestCommand
 import sys
 
-VERSION = "1.75"
+VERSION = "1.76"
 DESCRIPTION = """
 pita - pita improves transcript annotation
 """
